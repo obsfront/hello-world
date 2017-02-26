abf598dcf3409d1f91a63dd705fb85c2bfc865a0
C Cesar Nardo Junior <C Cesar Nardo Junior>
2017-02-25 23:04:09 -0300

    Workflow pronto: pau na máquina

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp');
 var pug = require('gulp-pug2');
 var sass = require('gulp-sass');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 // TASK
 gulp.task('pug', function(){
@@ -14,20 +17,29 @@ gulp.task('pug', function(){
 gulp.task('sass', function(){
   return gulp.src('./source/sass/*.scss')
   .pipe(sass())
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(rename('main.min.css'))
   .pipe(gulp.dest('./dist/css/'))
 })
 
 gulp.task('jshint', function(){
-  return gulp.src('./source/js/*.scss')
+  return gulp.src('./source/js/*.js')
   .pipe(jshint())
   .pipe(jshint.reporter('default'))
+})
+
+gulp.task('uglify', function(){
+  return gulp.src('./source/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/js/'))
 })
 
 // WATCH
 gulp.task('watch', function(){
   gulp.watch(['./source/*.pug'],['pug'])
   gulp.watch(['./source/sass/*.scss'],['sass'])
+  gulp.watch(['./source/js/*.js'],['uglify'])
 })
 
 // TASK PRINCIPAL
-gulp.task('default',['pug','sass','jshint','watch']);
+gulp.task('default',['pug','jshint','sass','uglify','watch']);
